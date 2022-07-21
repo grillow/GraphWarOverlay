481748a45ab8e7278093ea951166e0bc44feed96
grillow <spmr757@gmail.com>
2022-07-22 01:00:44 +0300

    logs

--- overlay.py
+++ overlay.py
@@ -19,6 +19,7 @@ WINDOW_HEIGHT = 450
 def left_click(event):
     x = (event.x / WINDOW_WIDTH) * MAP_WIDTH - MAP_WIDTH / 2
     y = -(event.y / WINDOW_HEIGHT) * MAP_HEIGHT + MAP_HEIGHT / 2
+    print("point clicked:\t{} {}".format(x, y))
     if len(points) != 0:
         if x < points[-1].x:
             x = points[-1].x
@@ -45,7 +46,8 @@ def right_click(event):
                 "+1/(1+exp(-{EXP}*(x-({px}))))*({dy})" \
                     .format(EXP=EXP, px=prev.x, dy=dy)
 
-    print(function)
+    if function != "":
+        print("\n{}\n".format(function))
     points.clear()
 
 
